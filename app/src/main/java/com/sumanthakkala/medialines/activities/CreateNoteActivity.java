7d123292aec7475eba244293ce445cbeb0438fa1
Sumanth Akkala <sumanth.akkala@gmail.com>
2020-07-04 02:33:09 -0400

    updated delay after speech recognition

--- app/src/main/java/com/sumanthakkala/medialines/activities/CreateNoteActivity.java
+++ app/src/main/java/com/sumanthakkala/medialines/activities/CreateNoteActivity.java
@@ -764,6 +764,7 @@ public class CreateNoteActivity extends AppCompatActivity implements  OnRequestP
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 2000);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi, Speak something.");
         try {
             startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
